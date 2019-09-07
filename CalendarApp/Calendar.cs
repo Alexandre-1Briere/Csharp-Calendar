12de9493cef5ad63b68ac982fa623332da9aa211
Alexandre-1Briere <infos@alexandrebriere.ca>
2019-09-07 08:28:37 -0400

    Initial code commit
    adding the first aprt of the ToString() function for the calendar, the main of the programms and the parameter of type DateTime in the class calendar to display the correct calendar

---
+++ CalendarApp/Calendar.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CalendarApp
+{
+    public class Calendar
+    {
+        private System.DateTime currentDate;
+        public DateTime CurrentDate => currentDate;
+
+        public Calendar()
+        {
+            currentDate = System.DateTime.Now;
+        }
+
+        
+        
+        //Author : Alexandre Brière
+        //Input : nothing
+        //Output : the string that correspond to the current selected calendar
+        //Description: override ToString to allow a display of the calendar directly by the function console.write or 
+        //                console.writeLine
+        public override string ToString()
+        {
+            string calendarStr = "====================================\n"+
+                                 "| D  | L  | M  | M  | J  | V  | S  |\n" + 
+                                 "====================================\n";
+
+            for (int i = 0; i < 6; i++)
+            {
+                
+            }
+            return calendarStr;
+        }
+    }
+}
---
+++ CalendarApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalendarApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Calendar());
+        }
+    }
+}
