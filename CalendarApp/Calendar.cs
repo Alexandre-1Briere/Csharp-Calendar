c4a2ff10c2a7d16eae0b412d7d48864f4feab820
Alexandre-1Briere <infos@alexandrebriere.ca>
2019-09-07 08:38:52 -0400

    Update Calendar.cs
    first test to display current month calendar

--- CalendarApp/Calendar.cs
+++ CalendarApp/Calendar.cs
@@ -21,13 +21,43 @@ namespace CalendarApp
         //                console.writeLine
         public override string ToString()
         {
+            //define the calendarStr as the header of the calendar at first.
             string calendarStr = "====================================\n"+
-                                 "| D  | L  | M  | M  | J  | V  | S  |\n" + 
+                                 "| S  | M  | T  | W  | T  | F  | S  |\n" + 
                                  "====================================\n";
-
+            
+            //for each fo the 6 row, we will build the string representing the row base on the currentDate.
+            int indexOfDay = 1;
+            int numberOfDay = DateTime.DaysInMonth(currentDate.Year, currentDate.Month);
+            DateTime firstDay = new DateTime(currentDate.Year, currentDate.Month, 1);
             for (int i = 0; i < 6; i++)
             {
-                
+                for (int j = 0; j < 7; j++)
+                {
+                    if (indexOfDay != numberOfDay)
+                    {
+                        if (i == 0 && j < firstDay.Day)
+                        {
+                            calendarStr += "|    ";
+                        }
+                        else if (indexOfDay < 9)
+                        {
+                            calendarStr += "| 0" + indexOfDay + " ";
+                            indexOfDay++;
+                        }
+                        else
+                        {
+                            calendarStr += "| " + indexOfDay + " ";
+                            indexOfDay++;
+                        }
+                    }
+                    else
+                    {
+                        calendarStr += "|    ";
+                    }
+                }
+
+                calendarStr += "|\n";
             }
             return calendarStr;
         }
